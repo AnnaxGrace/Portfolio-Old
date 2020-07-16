627525050a590910bae5759999aa0848902ce7ab
AnnaxGrace <anna.grace.conover@gmail.com>
2020-07-16 14:02:47 -0500

    made drop-down bigger and added projects as components

--- portfolio-react/src/components/pages/Portfolio.js
+++ portfolio-react/src/components/pages/Portfolio.js
@@ -3,10 +3,24 @@ import NavTabs from "../NavTabs"
 import "./portfoliostyle.css"
 import SideNav from "../SideNav/sideNav";
 import Carousel from "../Carousel/Carousel";
+import CookingTogether from "../CookingTogether/cookingtogether";
 
 function Portfolio() {
+
   function sidekick () {
       console.log("sidekick")
+  }
+
+  function cookingTogether () {
+    console.log("cooking")
+  }
+  
+  function once () {
+      console.log("once")
+  }
+
+  function scheduler () {
+      console.log("scheduler")
   }
 
   return (
@@ -18,98 +32,15 @@ function Portfolio() {
             </div>
 
             <div className="col-10 p-0">
-                <div className="row ">
-
-                    <div className="col-md-12 col-sm-12 col-xs-12 image-style">
-                        <div className="img-wrapper description">
-                            <img className="img-responsive" src={require("./Images/MealPlanner.png")} alt="MealPlanner"/>
-                            <div className="img-overlay">
-                                <button className="btn btnstyle" type = "button">
-                                    <div className="dropdown">
-                                        <button className="btn btn-secondary dropdown-toggle drop" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                           More Projects
-                                        </button>
-                                        <div className="dropdown-menu" aria-labelledby="dropdownMenuButton">
-                                            <button type="button" className="btn btn-link links dropdown-item" id="Self-Care Sidekick" onClick={sidekick}> Self-Care Sidekick </button>
-                                            <button type="button" className="btn btn-link links dropdown-item" id="Self-Care Sidekick" onClick={sidekick}> Self-Care Sidekick </button>
-                                            <button type="button" className="btn btn-link links dropdown-item" id="Self-Care Sidekick" onClick={sidekick}> Self-Care Sidekick </button>
-                                            <button type="button" className="btn btn-link links dropdown-item" id="Self-Care Sidekick" onClick={sidekick}> Self-Care Sidekick </button>
-
-
-                                        </div>
-                                    </div>
-                                    <p className="button-header">CookingTogether</p>
-                                    <a href= "https://annaxgrace.github.io/MealPlanner/" target = "_blank">Live Website</a>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-                                    <a href= "https://github.com/AnnaxGrace/MealPlanner" target = "_blank">GitHub Repository</a>
-                                </button>
-
-                                {/* <div id="project-description" className="show">
-                                    This one-stop-shop meal planning website tackles the issue of the mental load in households by allowing planning by multiple chefs.
-            
-                                    <ul>
-                                        <li>
-                                            Technologies used
-                                            <ul>
-                                                <li>
-                                                Front end uses HTML, CSS, and JavaScript/jQuery
-                                                </li>
-                                                <li>
-                                                  Back end uses node.js with express, passport, passport-session, nodemailer, bycrypt, mySql, and sequelize
-                                                </li>
-                                            </ul>
-                                        </li>
-                                        <li>
-                                            Guest Account
-                                            <ul>
-                                                <li>
-                                                    email: planner@mail.com
-                                                </li>
-                                                <li>
-                                                    password: Gue$tAcnt1
-                                                </li>
-                                            </ul>
-                                        </li>
-                                    </ul>
-                                </div> */}
-
-                            </div>
-                        </div>
-                    </div>
-                </div>
-
-
-    <div id="project-description">
-        <ul>
-            <li>
-                This one-stop-shop meal planning website tackles the issue of the mental load in households by allowing planning by multiple chefs.
-            </li>
-            <li>
-                Technologies used
-                <ul>
-                    <li>
-                        Front end uses HTML, CSS, and JavaScript/jQuery
-                    </li>
-                    <li>
-                        Back end uses node.js with express, passport, passport-session, nodemailer, bycrypt, mySql, and sequelize
-                    </li>
-                </ul>
-            </li>
-            <li>
-                Guest Account
-                <ul>
-                    <li>
-                        email: planner@mail.com
-                    </li>
-                    <li>
-                        password: Gue$tAcnt1
-                    </li>
-                </ul>
-            </li>
-        </ul>
-    </div>
-
-</div> 
-                {/* <Carousel /> */}
+                {/* <div className="row "> */}
+                    <CookingTogether 
+                    sidekick={sidekick}
+                    once={once}
+                    cooking={cookingTogether}
+                    scheduler={scheduler}
+                    />
+                    
+            </div>
             </div>
 
 
