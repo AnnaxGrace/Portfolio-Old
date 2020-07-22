8e57baa87a65648b33836057306a51f36e744b28
AnnaxGrace <anna.grace.conover@gmail.com>
2020-07-21 22:07:20 -0500

    added project.js

--- portfolio-react/src/components/pages/Portfolio.js
+++ portfolio-react/src/components/pages/Portfolio.js
@@ -1,105 +1,67 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import NavTabs from "../NavTabs"
 import "./portfoliostyle.css"
 import SideNav from "../SideNav/sideNav";
 import Carousel from "../Carousel/Carousel";
+import CookingTogether from "../CookingTogether/cookingtogether";
+import WhichProject from "../WhichProject.js/whichproject";
+
+// let project;
 
 function Portfolio() {
+
+
+  function sidekick () {
+    localStorage.setItem("project", "sidekick");
+    //   project = "sidekick"
+    window.location.reload();
+      console.log("sidekick")
+  }
+
+  function cookingTogether () {
+    localStorage.setItem("project", "cookingTogether");
+    window.location.reload();
+    // project = "cookingTogether"
+  }
+  
+  function once () {
+    localStorage.setItem("project", "once");
+    window.location.reload();
+    //   project = "once"
+  }
+
+  function scheduler () {
+    localStorage.setItem("project", "scheduler");
+    window.location.reload();
+    //   project = "scheduler"
+  }
+
   return (
     <div>
-        <div class="row">
+        <div className="row">
 
-            <div className="col-2 justify-content-center side">
+            <div className="col-2 justify-content-center side" >
                 <SideNav />
             </div>
 
             <div className="col-10 p-0">
-                <div className="row ">
-
-                    <div class="col-md-12 col-sm-12 col-xs-12 image-style">
-                        <div class="img-wrapper description">
-                            <img class="img-responsive" src={require("./Images/MealPlanner.png")} alt="MealPlanner"/>
-                            <div class="img-overlay">
-                                <button class = "btn btnstyle" type = "button">
-                                    <h5>CookingTogether</h5>
-                                    <a href= "https://annaxgrace.github.io/MealPlanner/" target = "_blank">Live Website</a>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-                                    <a href= "https://github.com/AnnaxGrace/MealPlanner" target = "_blank">GitHub Repository</a>
-                                </button>
-
-                                {/* <div id="project-description" className="show">
-                                    This one-stop-shop meal planning website tackles the issue of the mental load in households by allowing planning by multiple chefs.
-            
-                                    <ul>
-                                        <li>
-                                            Technologies used
-                                            <ul>
-                                                <li>
-                                                Front end uses HTML, CSS, and JavaScript/jQuery
-                                                </li>
-                                                <li>
-                                                  Back end uses node.js with express, passport, passport-session, nodemailer, bycrypt, mySql, and sequelize
-                                                </li>
-                                            </ul>
-                                        </li>
-                                        <li>
-                                            Guest Account
-                                            <ul>
-                                                <li>
-                                                    email: planner@mail.com
-                                                </li>
-                                                <li>
-                                                    password: Gue$tAcnt1
-                                                </li>
-                                            </ul>
-                                        </li>
-                                    </ul>
-                                </div> */}
-
-                            </div>
-                        </div>
-                    </div>
-                </div>
-
-
-    <div id="project-description" className="show">
-        <ul>
-            <li>
-                This one-stop-shop meal planning website tackles the issue of the mental load in households by allowing planning by multiple chefs.
-            </li>
-            <li>
-                Technologies used
-                <ul>
-                    <li>
-                        Front end uses HTML, CSS, and JavaScript/jQuery
-                    </li>
-                    <li>
-                        Back end uses node.js with express, passport, passport-session, nodemailer, bycrypt, mySql, and sequelize
-                    </li>
-                </ul>
-            </li>
-            <li>
-                Guest Account
-                <ul>
-                    <li>
-                        email: planner@mail.com
-                    </li>
-                    <li>
-                        password: Gue$tAcnt1
-                    </li>
-                </ul>
-            </li>
-        </ul>
-    </div>
-
-</div> 
-                {/* <Carousel /> */}
+                {/* <div className="row "> */}
+                    <WhichProject
+                    sidekick={sidekick}
+                    once={once}
+                    cookingTogether={cookingTogether}
+                    scheduler={scheduler}
+                    // project={project}
+                    />
+                    
+            </div>
             </div>
 
 
 
     
     <footer>
-            <p class="copyright">&copy; Anna Conover</p>
+            <p className="copyright">&copy; Anna Conover</p>
         </footer>
     </div>
   );
