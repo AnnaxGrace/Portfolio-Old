ab0c37636d863f258fbe144713907bd5f3638349
Pennington, Anna <aconover@teksystems.com>
2023-02-08 20:28:00 +0000

    updated navbar to raindrops

---
+++ portfolio-react/src/components/SideNav/sideNavcopy.js
@@ -0,0 +1,73 @@
+import React from "react"
+import WhichNav from "../whichNav/whichNav"
+import "./sideNavcopy.css"
+
+//component for our side nav bar
+function SideNav(props) {
+
+    return (
+        <div className="side-wrapper">
+            <div className="raindrops">
+            <div id="top-row">
+                {/* <div className="col"> */}
+                    <div class="raindrop">
+                        <p class="raintext">about me</p>
+                    {/* </div> */}
+                </div>
+                {/* <div className="col"> */}
+                    <div class="raindrop">
+                        <p class="raintext">certs</p>
+                    {/* </div> */}
+                </div>
+                {/* <div className="col"> */}
+                    <div class="raindrop">
+                        <p class="raintext">certs</p>
+                    </div>
+                </div>
+            </div>
+            <div id="mid-row">
+                {/* <div className="col"> */}
+                    <div class="raindrop">
+                        <p class="raintext">about me</p>
+                    {/* </div> */}
+                </div>
+                {/* <div className="col"> */}
+                    <div class="raindrop">
+                        <p class="raintext">certs</p>
+                    {/* </div> */}
+                </div>
+                {/* <div className="col">
+                    <div class="raindrop">
+                        <p class="raintext">certs</p>
+                    </div>
+                </div> */}
+            </div>
+
+
+            {/* <div className=" row justify-content-center icon-small">
+                <a href="https://github.com/AnnaxGrace" target="_blank" className="fab fa-github icon fa-3x d-flex " rel="noopener noreferrer"></a>
+                <a href="https://linkedin.com/in/anna-grace-conover" target="_blank" className="fab fa-linkedin icon fa-3x d-flex" rel="noopener noreferrer"></a>
+                <div class="git-link">
+                    <a href="mailto:annaxgrace2022@gmail.com" className="fas fa-envelope icon fa-3x d-flex" rel="noopener noreferrer"></a>
+                </div>
+            </div>
+
+            <div class="row justify-content-center mb-5">
+                <img src={require("../pages/Images/aws-certified-solutions-architect-associate.png")} alt="AWS SAA Certification icon" ></img>
+                <img src={require("../pages/Images/cert_mark_SP_small_150px.png")} alt="Safe Certification icon" ></img>
+                <img src={require("../pages/Images/hashicorp-certified-terraform-associate.png")} alt="Terraform associate Icon" ></img>
+            </div> */}
+
+            {/* determines which navigation links to show */}
+            {/* <div class="row justify-content-center">
+                <WhichNav
+                    aboutClick={props.aboutClick}
+                    aboutResume={props.aboutResume}
+                />
+            </div> */}
+
+        </div>
+    )
+}
+
+export default SideNav;
--- portfolio-react/src/components/pages/Landing.js
+++ portfolio-react/src/components/pages/Landing.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import "../Banner/banner.css";
 import "./landingstyle.css"
-import SideNav from "../SideNav/sideNav";
+import SideNav from "../SideNav/sideNavcopy";
 
 
 function Landing() {
@@ -35,36 +35,10 @@ function Landing() {
 
         <div class="container-fluid">
 
-            <p class="pt-5 top-name">ANNA GRACE PENNINGTON</p>
+            {/* <p class="pt-5 top-name">ANNA GRACE PENNINGTON</p> */}
 
             <div class="row">
-                <div className="col-md-6 col-sm-12 justify-content-center side">
-                    <div class="mt-5">
-                        {/* <p>
-                            Hello! My name is Anna Conover and I am a DevOps Engineer/Cloud Architect.
-                        </p> */}
-                        {/* <p>
-                            I am originally from Houston, Texas. I went to the University of Oklahoma, where I received a Bachelor's degree in Drama: Acting Emphasis. I am a person that likes learning new things and being challenged. I learn quickly, and work hard.
-                        </p> */}
-                        <p class="brand-statement">
-                            DevOps Engineer with a drive for creating code and processes that make the world a better place – focusing on modern and transformative work that consistently enriches the workplace and the community. I have a Bachelor’s Degree in Fine Arts, and as an actor I bring a unique understanding of collaboration, creativity, and storytelling into the industry. My ability and interest in challenges and puzzle-solving always lends itself to my building of a character for a play - and I use the same skills to write code and create infrastructure that is clean and efficient to build your story
-                        </p>
-                        {/* <p>
-                            In July of 2020, I received a certification from the UCLA coding Bootcamp program, where I learned multiple technical skills (including Javascript, CSS, HTML, mySql, Node, and React) while gaining experience in building projects with teams.
-                        </p> */}
-
-                        {/* <p>
-                            You can view my work through the "Portfolio" link. Please do not hesitate to reach out to me through email!
-                        </p> */}
-                    </div>
-                    {/* <SideNav
-                        aboutClick={aboutClick}
-                        aboutResume={aboutResume}
-                    /> */}
-
-                </div>
-
-                <div className="col-md-6 col-sm-12 p-0 mb-3">
+                <div className="col-lg-5 col-md-12 col-sm-12 p-0 mb-3">
                     <div class='cloud'>
                         <svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 1000 1000" enable-background="new 0 0 1000 1000" xmlSpace="preserve">
                             <metadata> Svg Vector Icons : http://www.onlinewebfonts.com/icon </metadata>
@@ -95,6 +69,11 @@ function Landing() {
 
 
                     </div>
+
+                    <SideNav
+                        aboutClick={aboutClick}
+                        aboutResume={aboutResume}
+                    />
 
                     {/* This div appears when the about link is clicked */}
                     <div id={aboutState} className="about">
@@ -127,11 +106,103 @@ function Landing() {
 
                     </div>
                 </div>
+                <div className="col-lg-7 col-md-12 col-sm-12 justify-content-center side">
+                    <div class="mt-5">
+                        {/* <p>
+                            Hello! My name is Anna Conover and I am a DevOps Engineer/Cloud Architect.
+                        </p> */}
+                        {/* <p>
+                            I am originally from Houston, Texas. I went to the University of Oklahoma, where I received a Bachelor's degree in Drama: Acting Emphasis. I am a person that likes learning new things and being challenged. I learn quickly, and work hard.
+                        </p> */}
+                        <p class="pt-5 top-name">ANNA GRACE PENNINGTON</p>
+                        <p class="brand-statement">
+                            I'm a DevOps Engineer with a drive for creating code and processes that make the world a better place – focusing on modern and transformative work that consistently enriches the workplace and the community. I have a Bachelor’s Degree in Fine Arts, and as an actor I bring a unique understanding of collaboration, creativity, and storytelling into the industry. My ability and interest in challenges and puzzle-solving always lends itself to my building of a character for a play - and I use the same skills to write code and create infrastructure that is clean and efficient to build your story
+                        </p>
+                        {/* <p>
+                            In July of 2020, I received a certification from the UCLA coding Bootcamp program, where I learned multiple technical skills (including Javascript, CSS, HTML, mySql, Node, and React) while gaining experience in building projects with teams.
+                        </p> */}
+
+                        {/* <p>
+                            You can view my work through the "Portfolio" link. Please do not hesitate to reach out to me through email!
+                        </p> */}
+                    </div>
+                    {/* <SideNav
+                        aboutClick={aboutClick}
+                        aboutResume={aboutResume}
+                    /> */}
+
+                </div>
+
+                {/* <div className="col-md-6 col-sm-12 p-0 mb-3">
+                    <div class='cloud'>
+                        <svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 1000 1000" enable-background="new 0 0 1000 1000" xmlSpace="preserve">
+                            <metadata> Svg Vector Icons : http://www.onlinewebfonts.com/icon </metadata>
+                            <g><g><path
+                                class="cloudDrawn"
+                                d="M808.5,772.2h-617c-79.9,0-145.2-65.3-145.2-145.2c0-69,47.2-127,112.5-141.6c-3.6-14.5-3.6-25.4-3.6-39.9c0-119.8,98-217.8,217.8-217.8c94.4,0,174.2,61.7,206.9,145.2c29-21.8,65.3-36.3,101.6-36.3c87.1,0,156.1,65.3,163.3,148.8C906.5,500,953.7,558.1,953.7,627C953.7,706.9,888.4,772.2,808.5,772.2z" />
+                            </g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g></g>
+                        </svg>
+
+                        <div class="bouncing-text">
+                            <div class="h">H</div>
+                            <div class="e">e</div>
+                            <div class="a">a</div>
+                            <div class="d">d</div>
+                            <div class="i">I</div>
+                            <div class="n">n</div>
+                            <div class="t">T</div>
+                            <div class="hh2">h</div>
+                            <div class="e2">e</div>
+                            <div class="c">C</div>
+                            <div class="l">l</div>
+                            <div class="o">o</div>
+                            <div class="u">u</div>
+                            <div class="d2">d</div>
+                            <div class="s">s</div>
+
+                        </div>
+
+
+                    </div>
+
+                    {/* This div appears when the about link is clicked */}
+                <div id={aboutState} className="about">
+                    <button type="button" class="btn btn-light btn-close" onClick={closeButton}>x</button>
+                    <div className="about-p">
+                        <p>
+                            Hello! My name is Anna Conover and I am a DevOps Engineer/Cloud Architect.
+                        </p>
+                        <p>
+                            I am originally from Houston, Texas. I went to the University of Oklahoma, where I received a Bachelor's degree in Drama: Acting Emphasis. I am a person that likes learning new things and being challenged. I learn quickly, and work hard.
+                        </p>
+                        <p>
+                            As an actor, I bring a unique understanding of collaboration, creativity, and storytelling into the developer world. My ability and interest in challenges and puzzle-solving always lends itself to my building of a character- and I use the same skills to write code that is clean, mobile-first, and efficient to build your story.
+                        </p>
+                        <p>
+                            In July of 2020, I received a certification from the UCLA coding Bootcamp program, where I learned multiple technical skills (including Javascript, CSS, HTML, mySql, Node, and React) while gaining experience in building projects with teams.
+                        </p>
+
+                        <p>
+                            You can view my work through the "Portfolio" link. Please do not hesitate to reach out to me through email!
+                        </p>
+                    </div>
+                </div>
+
+                {/* This div appears when the resume link is clicked */}
+                <div id={resumeState} className="resume">
+                    <button type="button" class="btn btn-light btn-close-resume" onClick={closeButton}>x</button>
+                    <p class="mobile-download">Hey! Looks like you're looking at this screen on mobile. Download the PDF <a id="mobile-download-here" target="_blank" href="https://drive.google.com/file/d/1OGL_IKpI9Lo0efmMWescbduoHlTOfIGt/view?usp=sharing">here</a>, or check out this page in a desktop!</p>
+                    <embed class="pdf resume-no-show" src={require("../pages/Images//Anna Grace Conover Portfolio Resume.pdf")} width="800px" height="700px" />
+
+                </div>
+                {/* </div> */}
 
             </div>
 
+
+
             {/* <p>ANNA GRACE PENNINGTON</p> */}
-            <div class="container">
+            {/* <div class="container">
                 <nav class="navbar navbar-expand-lg fixed-bottom bottom-nav placement">
                     <div class="row">
                         <div class="col-md-3">
@@ -149,8 +220,9 @@ function Landing() {
                         </div>
                     </div>
                 </nav>
-                {/* <div>Icon made from <a href="http://www.onlinewebfonts.com/icon">Icon Fonts</a> is licensed by CC BY 3.0</div> */}
-            </div>
+            </div> */}
+            {/* <div>Icon made from <a href="http://www.onlinewebfonts.com/icon">Icon Fonts</a> is licensed by CC BY 3.0</div> */}
+
         </div>
     )
 }
